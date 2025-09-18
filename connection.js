542557f04e2f7d922d535d3a83cc553a317bf0d7
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-09-18 13:21:31 +0100

    connect

--- connection.js
+++ connection.js
@@ -25,24 +25,24 @@ import data from './locals.js'
 const pg = await import('pg')
 
 // const connection = new pg.Pool({
-// connectionString: data.E_HOST,
-// ssl: true,
-// max: 20, // set pool max size to 20
-// idleTimeoutMillis: 5000, // close idle clients after 5 seconds
-// // connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-// maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+//   user: data.DB_USER || 'root',
+//   database: data.DATABASE || 'HealthWa',
+//   password: data.PASSWORD || '',
+//   port: data.DB_PORT || 5432,
+//   // ssl: true,
+//   max: 20, // set pool max size to 20
+//   idleTimeoutMillis: 1000, // close idle clients after 1 second
+//   connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+//   maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
 // })
 
 const connection = new pg.Pool({
-  user: data.DB_USER || 'root',
-  database: data.DATABASE || 'HealthWa',
-  password: data.PASSWORD || '',
-  port: data.DB_PORT || 5432,
-  // ssl: true,
-  max: 20, // set pool max size to 20
-  idleTimeoutMillis: 1000, // close idle clients after 1 second
-  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-  maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+connectionString: data.E_HOST,
+ssl: true,
+max: 20, // set pool max size to 20
+idleTimeoutMillis: 5000, // close idle clients after 5 seconds
+// connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
 })
 
 
