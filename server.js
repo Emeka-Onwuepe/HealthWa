0b21392a95a0183266fa7b4307b78348acc10cad
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-09-18 01:54:40 +0100

    cors

--- server.js
+++ server.js
@@ -17,8 +17,17 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");  
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");  // allow access from localhost:3000
+  res.header("Access-Control-Allow-Origin", "http://localhost:8081");  // allow access from localhost:8081
+
   next();
 });
+
+// cors middleware for localhost:3000
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");  // allow access from localhost:3000
+//   next();
+// });
 
 // Body parser middleware
 app.use(express.json());
