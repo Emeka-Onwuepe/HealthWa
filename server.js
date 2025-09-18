b1cb16456a45f41d24e13c48b4cd9b480bb56df7
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-09-18 02:41:45 +0100

    cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 // add allowed hosts
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:8081'],
+  origin: 'http://localhost:8081',
   optionsSuccessStatus: 200,
 }));
 
