2c469c897cc91964100c1d2875a07898c07948b1
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-10-06 14:54:53 +0100

    returning the right data

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 export const getQueryData = (querydata,list=false) =>{
     // console.log(querydata)
     let data = querydata.rows
+    
+    if (data.length < 1) return null
 
     if (data.length == 1 && !list)
         data = data[0]
-
-    console.log('data from helper',data)
     return data 
 }
